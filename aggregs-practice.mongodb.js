195b892831bc49dbc62f72c5ba531fb6cd454869
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-21 19:56:47 +0530

    lookup operator in mongoDB aggreg pipelines

--- aggregs-practice.mongodb.js
+++ aggregs-practice.mongodb.js
@@ -169,7 +169,7 @@ db.users.aggregate([
 	},
 	{
 		$project: {
-			name: 1,
+			name: 1, // 1 means to include the attribute in results...
 			age: 1,
 		},
 	},
@@ -190,40 +190,94 @@ db.users.aggregate([
 	},
 ]);
 
-// 13) 
+// 13) list who registered most recently...
+db.users.aggregate([
+	{
+		$sort: {
+			registered: -1,
+		},
+	},
+	{
+		$limit: 4,
+	},
+	{
+		$project: {
+			name: 1,
+			registered: 1,
+			age: 1,
+		},
+	},
+]);
 
-// const q1 = db.users.aggregate(
-//     [
-//         {
-//           $group: {
-//             _id: "$favoriteFruit",
-//             countFruits: {
-//               $sum: 1   // means, adds "1" to individuals, when found...
-//             }
-//           }
-//         },
-//         {
-//           $sort: {
-//             countFruits: -1   // -1 means desc, and 1 means asc order...
-//           }
-//         },
-//         {
-//           $limit: 2
-//         }
-//     ]
-// )
-// console.log(q1);
+// 14) categorize users by their fav fruits...
+db.users.aggregate([
+	{
+		$group: {
+			_id: "$favoriteFruit",
+			resUsers: {
+				$push: "$name",
+			},
+		},
+	},
+]);
 
-// const q2 = db.users.aggregate(
-//     [
-//         {
-//           $group: {
-//             _id: "$favoriteFruit",
-//             countFruits: {
-//               $sum: 1   // means, adds "1" to individuals, when found...
-//             }
-//           }
-//         }
-//     ]
-// )
-// console.log(q2);
+// 15) how many users have "ad" as their 2nd tag in list of tags...
+db.users.aggregate([
+	{
+		$match: {
+			"tags.1": "ad", // .1 refers to 1st index...
+		},
+	},
+	{
+		$count: "totalResults",
+	},
+]);
+
+// 16) find users who has both "enim" and "id" in their tags...
+db.users.aggregate([
+	{
+		$match: {
+			tags: {
+				$all: ["enim", "id"],
+			},
+		},
+	},
+]);
+
+// 17) list all the companies located in USA with their corresponding user count...
+db.users.aggregate([
+	{
+		$match: {
+			"company.location.country": "USA",
+		},
+	},
+	{
+		$group: {
+			_id: "$company.title",
+			// _id: null,
+			userCount: {
+				$sum: 1,
+			},
+		},
+	},
+]);
+
+// 18) get author details from author_id, using lookup operator...
+db.books.aggregate([
+	{
+		$lookup: {
+			from: "authors",
+			localField: "author_id",
+			foreignField: "_id",
+			as: "author_details",
+		},
+	},
+	{
+		$addFields: {
+			author_details: {
+				// $first: "$author_details"
+				$arrayElemAt: ["$author_details", 0],
+			},
+		},
+	},
+]);
